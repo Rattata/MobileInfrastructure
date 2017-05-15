e8c76bf953cead9a80a2a32dcce08ac9cb0b5659
Sieger Falkena <Sieger@ATHENA>
2017-05-15 15:20:58 +0200

    First assignment (untested)

--- AndroidApp/app/src/main/java/com/example/app/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/app/androidapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.app.androidapp;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,9 +11,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.logging.Logger;
+
+import Infrastructure.EmailSender;
+import Infrastructure.GMail;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView mTextMessage;
+
+    private SensorManager sensorManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,11 +46,21 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-    private SensorManager sensor_manager;
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+
 
         setContentView(R.layout.activity_main);
 
@@ -46,4 +69,34 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER &&  event.values[2] < 0.5){
+            ArrayList<String> toAddresses = new ArrayList<>();
+            toAddresses.add("owner@phone.com");
+
+            GMail gmail = new GMail("siegerfalkena@gmail.com", "redacted", toAddresses, "Subject: phone drop :(", "you dropped your phone" );
+            try{
+                gmail.sendEmail();
+            }
+            catch(Exception except){
+                Logger log = Logger.getLogger(MainActivity.class.getName());
+                log.severe("could not send email!");
+            }
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Sensor accelMeter = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelMeter, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
 }
