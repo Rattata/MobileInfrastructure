6faeeb0ed4b80925b3fbb2fd9926e21ba4e61f94
Ryno Tseng <Ryno.Tseng@hva.nl>
2017-05-30 21:23:32 +0200

    Added barcode scan functionality to Android app, removed class assignment due to conflicts.

--- AndroidApp/app/src/main/java/com/example/app/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/app/androidapp/MainActivity.java
@@ -1,49 +1,61 @@
 package com.example.app.androidapp;
 
+import android.content.Intent;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    TextView barcodeText;
+    Button barcodeButton;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
-
-    private SensorManager sensor_manager;
+    //private SensorManager sensor_manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        //sensor_manager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        barcodeText = (TextView)findViewById(R.id.barcode_text);
+        barcodeButton = (Button)findViewById(R.id.barcode_button);
     }
 
+    public void scanBarcodeClick(View view){
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 0:
+                if (resultCode == CommonStatusCodes.SUCCESS){
+                    if (data != null){
+                        Barcode barcode = data.getParcelableExtra("barcode");
+                        barcodeText.setText(barcode.displayValue);
+                        postBarcode(barcode.displayValue);
+                    }
+                    else{
+                        barcodeText.setText("No barcode found");
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void postBarcode(String barcode) {
+        
+    }
 }
