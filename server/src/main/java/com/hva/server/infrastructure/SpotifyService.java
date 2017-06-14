69cb6b9b16e06bf1508d7d0f29a2f81747d9cc75
Sieger Falkena <Sieger@ATHENA>
2017-06-14 23:15:47 +0200

    fix refresh token logic

--- server/src/main/java/com/hva/server/infrastructure/SpotifyService.java
+++ server/src/main/java/com/hva/server/infrastructure/SpotifyService.java
@@ -16,6 +16,7 @@ import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.dao.Dao;
 import com.wrapper.spotify.Api;
 import com.wrapper.spotify.models.AuthorizationCodeCredentials;
+import com.wrapper.spotify.models.RefreshAccessTokenCredentials;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Logger;
@@ -33,7 +34,8 @@ public class SpotifyService {
     private final String ClientSecret = "e34f7b0c3e1d4bf0ae596d7007945e44";
     private final String callBack = "http://192.168.0.100:8080/auth/redirect";
 
-    @Context UriInfo uriInfo;
+    @Context
+    UriInfo uriInfo;
     private AccountRepository _accountRepo;
     private Api api;
     private Gson gson = new Gson();
@@ -58,22 +60,27 @@ public class SpotifyService {
         return request;
     }
 
-    private Account SetAuthToken(Account account) throws Exception {
-        AuthorizationCodeCredentials credentials = null;
+    private Account GetAuthToken(Account account) throws Exception {
 
-        credentials = api.authorizationCodeGrant(account.code).build().get();
-        api.setAccessToken(credentials.getAccessToken());
-        api.setRefreshToken(credentials.getRefreshToken());
-
-        account.access_token = credentials.getAccessToken();
-        account.refresh_token = credentials.getRefreshToken();
-        account.access_token_expires = GetExpiryDate(credentials.getExpiresIn());
+        if (account.refresh_token != null) {
+            RefreshAccessTokenCredentials cred = api.refreshAccessToken().build().get();
+            account.access_token = cred.getAccessToken();
+            account.access_token_expires = GetExpiryDate(cred.getExpiresIn());
+        } else {
+            AuthorizationCodeCredentials credentials = null;
+            credentials = api.authorizationCodeGrant(account.code).build().get();
+            api.setAccessToken(credentials.getAccessToken());
+            api.setRefreshToken(credentials.getRefreshToken());
+            account.access_token = credentials.getAccessToken();
+            account.refresh_token = credentials.getRefreshToken();
+            account.access_token_expires = GetExpiryDate(credentials.getExpiresIn());
+        }
         _accountRepo.CreateOrUpdate(account);
         return account;
     }
 
     public String queryAlbums(Account account, String barcode) throws AccountCodeExpiredException, Exception {
-        Account updatedAccount  = SetTokens(account);
+        Account updatedAccount = SetTokens(account);
 //        System.out.println(api.searchAlbums(String.format("nicki", barcode)).limit(1).build().getJson());
         System.out.println("\n");
         System.out.println(api.searchAlbums(String.format("%s", barcode)).limit(1).build().getJson());
@@ -84,7 +91,7 @@ public class SpotifyService {
         return "";
     }
 
-    public Account SetTokens(Account account) throws AccountCodeExpiredException, Exception {
+    public Account SetTokens(Account account) throws Exception {
 
         api.setAccessToken(account.access_token);
         api.setRefreshToken(account.refresh_token);
@@ -93,19 +100,9 @@ public class SpotifyService {
             Logger.getLogger(SpotifyService.class.getCanonicalName()).severe("account has no code: " + account.email);
         }
 
-        if (account.access_token == null || account.access_token_expires == null) {
-            updatedAccount = SetAuthToken(account);
-        }
-
-        Calendar date = Calendar.getInstance();
-        long t = date.getTimeInMillis();
-        Date Slackydate = new Date(t + 10000);
-
         Date now = new Date(Calendar.getInstance().getTimeInMillis());
-        if (account.access_token_expires.before(now)) {
-            api.refreshAccessToken();
-        } else if (account.access_token_expires.before(Slackydate)) {
-            api.refreshAccessToken();
+        if (account.access_token == null || account.access_token_expires == null || account.access_token_expires.before(now)) {
+            updatedAccount = GetAuthToken(account);
         }
 
         return updatedAccount;
